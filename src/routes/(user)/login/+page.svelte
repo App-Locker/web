703b19040e49cef6fb1264ebeeee00fce443e454
melurann <fruroa21@htl-kaindorf.at>
2025-01-30 13:36:46 +0100

    remove logout button from login form

--- src/routes/(user)/login/+page.svelte
+++ src/routes/(user)/login/+page.svelte
@@ -78,15 +78,6 @@
 				>
 					{isSubmitting ? 'Sining in...' : 'Sing In'}
 				</button>
-
-				<!-- Logout Button -->
-				<button
-					type="button"
-					on:click={() => user.logout()}
-					class="inline-flex h-10 items-center justify-center gap-2 rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground disabled:opacity-50"
-				>
-					Logout
-				</button>
 			</form>
 		</div>
 	</div>
