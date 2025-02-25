996e1dec22a897f5fbb39584ab69b9962cc3fee3
melurann <fruroa21@htl-kaindorf.at>
2025-02-25 21:57:30 +0100

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<main class="flex h-screen flex-col items-center justify-center p-8">
+	<div class="flex flex-col gap-8 text-center">
+		<div class="flex flex-col">
+			<h1 class="text-6xl font-bold">404</h1>
+			<p class="text-lg">PAGE NOT FOUND!</p>
+		</div>
+
+		<!-- border used as seperator -->
+		<div class="border"></div>
+		<div class="flex max-w-lg flex-col gap-4">
+			<p class="text-sm text-gray-400">
+				If you keep staring at that 404 page, you might just become one. But hey, at least you'll
+				finally find what you're looking for... right?
+			</p>
+			<a
+				href="/"
+				class="rounded-md bg-slate-700 px-4 py-2 font-medium text-white shadow-md hover:bg-slate-800"
+			>
+				Go Back Home
+			</a>
+		</div>
+	</div>
+</main>
