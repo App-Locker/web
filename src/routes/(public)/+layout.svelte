c5c18b4f6d51ba71b7c5c6ddd27c5af79f022654
melurann <fruroa21@htl-kaindorf.at>
2025-02-26 10:28:00 +0100

    add mobile menu

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
 	import { user } from '$lib/store/user.svelte';
+	import { MenuIcon } from 'lucide-svelte';
 	import { slide } from 'svelte/transition';
 
 	let { children } = $props();
 	let profileOpen = $state(false);
+	let mobileOpen = $state(false);
 </script>
 
 <!--Navigation Bar-->
 <header class="border-b bg-background/80 p-6 lg:px-8">
 	<nav class="flex items-center justify-between">
+		<!-- Mobile Menu -->
+		<div class="flex items-center gap-4 sm:hidden">
+			<a href="/"><img class="h-8 w-auto" src="logo.svg" alt="AppLocker Logo" /></a>
+			<button onclick={() => (mobileOpen = !mobileOpen)}>
+				<MenuIcon />
+			</button>
+		</div>
+
 		<!--Left-->
-		<div class="flex items-center gap-4">
+		<div class="hidden items-center gap-4 sm:flex">
 			<a href="/"><img class="h-8 w-auto" src="logo.svg" alt="AppLocker Logo" /></a>
 			<a class="text-sm font-medium leading-6" href="/#about">About</a>
 			<a class="text-sm font-medium leading-6" href="/download">Download</a>
@@ -19,13 +29,13 @@
 
 		<!--Right-->
 		{#if !user.value}
-			<div class="flex items-center gap-4">
+			<div class="hidden items-center gap-4 sm:flex">
 				<a class="text-sm font-medium leading-6" href="/login">Login</a>
 				<a class="text-sm font-medium leading-6" href="/register">Register</a>
 			</div>
 		{:else}
 			<div class="flex items-center gap-4">
-				<a class="text-sm font-medium leading-6" href="/dashboard">Dashboard</a>
+				<a class="hidden text-sm font-medium leading-6 sm:block" href="/dashboard">Dashboard</a>
 				<button
 					onclick={() => (profileOpen = !profileOpen)}
 					class="rounded-full border px-3.5 py-1.5 text-sm font-medium leading-6"
@@ -38,10 +48,17 @@
 
 <!--Profile Dropdown-->
 {#if profileOpen}
-	<div transition:slide class="absolute right-0.5 z-50 mt-0.5 min-w-48 rounded-md border">
+	<div
+		transition:slide
+		class="absolute right-0.5 z-50 mt-0.5 min-w-48 rounded-md border bg-white/95"
+	>
 		<div class="flex flex-col items-start space-y-1 px-2 pb-3 pt-2">
 			<a class="w-full px-4 py-2 text-sm font-medium leading-6 hover:bg-slate-100" href="/settings"
 				>Settings</a
+			>
+			<a
+				class="w-full px-4 py-2 text-sm font-medium leading-6 hover:bg-slate-100 sm:hidden"
+				href="/dashboard">Dashboard</a
 			>
 			<button
 				class="w-full px-4 py-2 text-left text-sm font-medium leading-6 hover:bg-slate-100"
@@ -52,6 +69,23 @@
 				}}>Logout</button
 			>
 		</div>
+	</div>
+{/if}
+
+<!-- Mobile Menu Dropdown -->
+{#if mobileOpen}
+	<div
+		transition:slide
+		class="left-0 z-50 flex w-full flex-col justify-center gap-4 border-b bg-white p-8 text-center sm:hidden"
+	>
+		<a class="text-sm font-medium leading-6" href="/#about">About</a>
+		<a class="text-sm font-medium leading-6" href="/download">Download</a>
+		<a class="text-sm font-medium leading-6" href="/changelog">Changelog</a>
+
+		{#if !user.value}
+			<a class="text-sm font-medium leading-6" href="/login">Login</a>
+			<a class="text-sm font-medium leading-6" href="/register">Register</a>
+		{/if}
 	</div>
 {/if}
 
