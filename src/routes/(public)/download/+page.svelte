9115e314412904d472e4d1dbd7d372a33aeac1f5
melurann <fruroa21@htl-kaindorf.at>
2025-02-26 12:50:47 +0100

    add download page

---
+++ src/routes/(public)/download/+page.svelte
@@ -0,0 +1,36 @@
+<div class="flex flex-col gap-32 p-8 sm:px-16 sm:py-32">
+	<div class="max-w-4xl space-y-8">
+		<div class="space-y-2">
+			<h1 class="text-4xl font-bold text-slate-900 sm:text-6xl">Downlaod AppLocker</h1>
+			<h2 class="text-2xl text-slate-900 sm:text-4xl">
+				Get the latest version of AppLocker for your Windows device.
+			</h2>
+		</div>
+
+		<a
+			class="flex h-12 max-w-sm items-center justify-center rounded-lg bg-blue-700 text-white"
+			href="/downloads/applocker.txt"
+			download="applocker.txt"
+			>Downlaod for Windows
+		</a>
+	</div>
+
+	<div class="flex flex-col justify-center gap-4 sm:flex-row">
+		<div class="rounded-lg border p-8 shadow-lg sm:p-16">
+			<h1 class="text-2xl font-bold">Want to know more about AppLocker?</h1>
+			<p>
+				You can learn more about uns and our goals <a class="text-blue-700 underline" href="/#about"
+					>here</a
+				>
+			</p>
+		</div>
+		<div class="rounded-lg border p-8 shadow-lg sm:p-16">
+			<h1 class="text-2xl font-bold">Want to know more about AppLocker?</h1>
+			<p>
+				You can learn more about uns and our goals <a class="text-blue-700 underline" href="/#about"
+					>here</a
+				>
+			</p>
+		</div>
+	</div>
+</div>
