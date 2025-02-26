559d76ae95e0671e377fbbd0f0648359a2f7561b
melurann <fruroa21@htl-kaindorf.at>
2025-02-26 13:04:01 +0100

    add Changelog component

---
+++ src/lib/components/Changelog.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		title: string;
+		releaseDate: string;
+		children: Snippet;
+	};
+
+	let { title, releaseDate, children }: Props = $props();
+</script>
+
+<div class="grid grid-cols-1 gap-4 py-4 md:grid-cols-3">
+	<div class="">
+		<h2 class="text-xl font-semibold uppercase text-slate-900 sm:text-2xl">
+			{title}
+		</h2>
+		<p>{releaseDate}</p>
+	</div>
+
+	<div class="md:col-span-2">
+		{@render children()}
+	</div>
+</div>
